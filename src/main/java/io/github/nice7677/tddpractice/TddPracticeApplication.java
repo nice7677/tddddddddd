ed26bf654b18485427b67c84987741da0caf465a
진우 <nice7677@naver.com>
2024-04-08 20:51:07 +0900

    init

---
+++ src/main/java/io/github/nice7677/tddpractice/TddPracticeApplication.java
@@ -0,0 +1,13 @@
+package io.github.nice7677.tddpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TddPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TddPracticeApplication.class, args);
+    }
+
+}
